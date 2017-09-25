3fc2d92252718b5ec0e95944f9f42a6625b51fbd
summer-weimin <32190685+summer-weimin@users.noreply.github.com>
2017-09-25 21:23:11 +0800

    这是第二次修改数据

--- gitFirstTest/Program.cs
+++ gitFirstTest/Program.cs
@@ -11,6 +11,17 @@ namespace gitFirstTest
         static void Main(string[] args)
         {
             Console.WriteLine("这是创建github使用的");
+            person p = new person();
+            p.age = 21;
+            Student s = new Student();
+            s.name = "hello";
         }
+    }
+
+    public class person
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
     }
 }
---
+++ gitFirstTest/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitFirstTest
+{
+  public   class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
